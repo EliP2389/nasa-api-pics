3ae9df55203f6cb9e23ea65d8e8154900f36e344
Eli Pagan <mbk8689@gmail.com>
2022-10-13 08:05:12 -0400

    function getNasaImages

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 // API URL
-const count = 10
-const apiKey = 'DEMO_KEY'
-const apiURL = `https://api.nasa.gov/planetary/apod?_key=${apiKey}&count=${count}`
+const count = 10;
+const apiKey = "DEMO_KEY";
+const apiURL = `https://api.nasa.gov/planetary/apod?_key=${apiKey}&count=${count}`;
 
-nasaPod = () => {}
+let resultsArray = [];
+
+// Get Images from NASA API
+async function getNasaImages() {
+    try {
+        
+    } catch (error) {
+        // Catch Error Here
+    }
+}
