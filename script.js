d11a789d4355ad94a1232f2a6e75bfd9ee7ee9f1
Eli Pagan <mbk8689@gmail.com>
2022-10-13 10:55:14 -0400

    added constants to target our html elements

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
+const resultsNav = document.getElementById("resultsNav");
+const favoritesNav = document.getElementById("favoritesNav");
+const imageContainer = document.querySelector(".images-Container");
+const saveConfirmed = document.querySelector(".save-confirmed");
+const loader = document.querySelector(".loader");
+
 // API URL
 const count = 10;
-const apiKey = 'DEMO_KEY';
+const apiKey = "DEMO_KEY";
 const apiURL = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&count=${count}`;
 
 let resultsArray = [];
 
 // Get Images from NASA API
 async function getNasaImages() {
-    try {
-        const response = await fetch(apiURL);
-        resultsArray = await response.json();
-        console.log(resultsArray);
-    } catch (error) {
-        // Catch Error Here
-    }
+  try {
+    const response = await fetch(apiURL);
+    resultsArray = await response.json();
+    console.log(resultsArray);
+  } catch (error) {
+    // Catch Error Here
+  }
 }
 
 // On Load
