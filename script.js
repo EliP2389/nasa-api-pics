a941fae9ed5befe511db41429207fdb509650750
Eli Pagan <mbk8689@gmail.com>
2022-10-13 10:50:11 -0400

    deleted card div, so we can dinamically create it

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 // API URL
 const count = 10;
-const apiKey = "DEMO_KEY";
-const apiURL = `https://api.nasa.gov/planetary/apod?_key=${apiKey}&count=${count}`;
+const apiKey = 'DEMO_KEY';
+const apiURL = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&count=${count}`;
 
 let resultsArray = [];
 
 // Get Images from NASA API
 async function getNasaImages() {
     try {
-        
+        const response = await fetch(apiURL);
+        resultsArray = await response.json();
+        console.log(resultsArray);
     } catch (error) {
         // Catch Error Here
     }
 }
+
+// On Load
+getNasaImages();
