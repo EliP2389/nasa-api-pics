fc9286e142af2c86d4d4bcd7d0fae3db018f0b44
Eli Pagan <mbk8689@gmail.com>
2022-10-19 12:43:49 -0400

    created a conditional in the savefavorites function to check for savedfavorites and added it to our favorites

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-const resultsNav = document.getElementById('resultsNav');
-const favoritesNav = document.getElementById('favoritesNav');
-const imageContainer = document.querySelector('.images-container');
-const saveConfirmed = document.querySelector('.save-confirmed');
-const loader = document.querySelector('.loader');
+const resultsNav = document.getElementById("resultsNav");
+const favoritesNav = document.getElementById("favoritesNav");
+const imageContainer = document.querySelector(".images-container");
+const saveConfirmed = document.querySelector(".save-confirmed");
+const loader = document.querySelector(".loader");
 
 // API URL
 const count = 10;
@@ -10,7 +10,7 @@ const apiKey = "DEMO_KEY";
 const apiURL = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&count=${count}`;
 
 let resultsArray = [];
-let Favorites = {};
+let favorites = {};
 
 // Update DOM
 updateDOM = () => {
@@ -40,7 +40,7 @@ updateDOM = () => {
     const saveText = document.createElement("p");
     saveText.classList.add("clickable");
     saveText.textContent = "Add To Favorites";
-    saveText.setAttribute('onclick', `saveFavorite('${result.url}')`);
+    saveText.setAttribute("onclick", `saveFavorite('${result.url}')`);
     // Card Text
     const cardText = document.createElement("p");
     cardText.textContent = result.explanation;
@@ -51,7 +51,8 @@ updateDOM = () => {
     const date = document.createElement("strong");
     date.textContent = result.date;
     // Copyright
-    const copyrightResult = result.copyright === undefined ? '' : result.copyright;
+    const copyrightResult =
+      result.copyright === undefined ? "" : result.copyright;
     const copyright = document.createElement("span");
     copyright.textContent = ` ${copyrightResult}`;
     // Append
@@ -69,16 +70,22 @@ async function getNasaImages() {
     const response = await fetch(apiURL);
     resultsArray = await response.json();
     console.log(resultsArray);
-    updateDOM()
+    updateDOM();
   } catch (error) {
     // Catch Error Here
   }
 }
 
 // Add result to favorites
-saveFavorite = (itemUrl) =>  {
-    console.log(itemUrl);
-}
+saveFavorite = (itemUrl) => {
+  // Loop through Results array to select favorites
+  resultsArray.forEach((item) => {
+    if (item.url.includes(itemUrl)) {
+      favorites[itemUrl] = item;
+      console.log(favorites);
+    }
+  });
+};
 
 // On Load
 getNasaImages();
